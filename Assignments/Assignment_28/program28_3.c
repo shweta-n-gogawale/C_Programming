1752ac3dcefb6d4db7fea27abdd1dd39b00c5e2f
Shweta Gogawale <gogawaleshweta12@gmail.com>
2025-11-25 11:32:07 +0530

    Added Assignment 28 solutions

---
+++ Assignments/Assignment_28/program28_3.c
@@ -0,0 +1,38 @@
+/*
+    Function Name   : Pattern
+    Description     : Accept rows and columns and print letters row-wise (AAAA, BBBB, CCCC...)
+    Input           : Integer iRow, Integer iCol
+    Output          : Pattern
+    Author          : Shweta Gogawale
+    Date            : 2025-11-24
+*/
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+    char ch = 'A';
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            printf("%c\t", ch);
+        }
+        ch++;
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns\n");
+    scanf("%d %d",&iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
