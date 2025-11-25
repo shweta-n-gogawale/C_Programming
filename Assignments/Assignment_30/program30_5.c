367d24ee1ed2e72fb7e37a08d9522405669860e8
Shweta Gogawale <gogawaleshweta12@gmail.com>
2025-11-25 11:34:35 +0530

    Added Assignment 30 solutions

---
+++ Assignments/Assignment_30/program30_5.c
@@ -0,0 +1,43 @@
+/*
+    Function Name   : Pattern
+    Description     : Display numbers in 1st and last row & column; middle is '*'.
+    Input           : Integer iRow, Integer iCol
+    Output          : Pattern
+    Author          : Shweta Gogawale
+    Date            : 2025-11-25
+*/
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(i == 1 || i == iRow || j == 1 || j == iCol)
+            {
+                printf("%d\t", j);
+            }
+            else
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns\n");
+    scanf("%d %d",&iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
